f21f1e2784ee0ac51febe42c0937bcf832628cfe
Anthony Mittaz <sync@me.com>
2010-09-13 21:55:59 +1000

    used template

---
+++ Source/Main/Views/RootViewController.h
@@ -0,0 +1,14 @@
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+@private
+    NSFetchedResultsController *fetchedResultsController_;
+    NSManagedObjectContext *managedObjectContext_;
+}
+
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+
+@end
